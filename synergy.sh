fabb9db9a3501e0e691cb549aa0e6f7ccd450d20
cphyc <admin@cphyc.me>
2014-06-09 01:53:05 -0700

    Missing ref to original author

--- synergy.sh
+++ synergy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-####################################################
-# This script was created by cphyc.                #
-#                                                  #
-# Whoever finds it interesting shall buy me a      #
-# beer or any drink if he is interested in         #
-# having fun with me. Otherwise, he can just       #
-# use it, modify it and add my name to his fork    #
-# (if he wants to).                                #
-####################################################
+#####################################################
+# This script was created by cphyc and adapted from #
+# http://hang4r.blogspot.com/2011/06/control-synergy-server-and-its-clients.html
+#                                                   #
+# Whoever finds it interesting shall buy me a       #
+# beer or any drink if he is interested in          #
+# having fun with me. Otherwise, he can just        #
+# use it, modify it and add my name to his fork     #
+# (if he wants to).                                 #
+#####################################################
 
 # Start the synergy server daemon and sleep to ensure it's up
 # prior to kicking off our uber-simple client initialization
