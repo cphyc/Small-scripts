e596d37664512bd971834d5f5234b10ff521b227
cphyc <admin@cphyc.me>
2014-09-23 14:36:12 +0200

    Added check to prevent the computer from going under 5% of battery

--- battery.sh
+++ battery.sh
@@ -10,5 +10,17 @@
 # (if he wants to).                                #
 ####################################################
 
+MIN_BATTERY=5
+WARN_BATTERY=10
+BATTERY=`acpi -b | cut -d ',' -f 2 | cut -d '%' -f 1 | cut -d " " -f 2`
+BATTERY_PREV=`[ -e /tmp/running_low ] && cat /tmp/running_low || 0`
+BATTERY_STATE=`acpi -b | cut -d ',' -f 1 | cut -d ':' -f 2 | cut -d " " -f 2`
 
-acpi -b | cut -d ',' -f 2 | cut -d '%' -f 1 | cut -d " " -f 2
+if [ $BATTERY_STATE == "Discharging" ]; then
+    # Checks that the battery hasnot reached the minimum
+    [ $BATTERY -lt $WARN_BATTERY ] && echo $BATTERY > /tmp/running_low &&
+    [ $BATTERY -lt $BATTERY_PREV ] && zenity --info --text="Battery low ($BATTERY%)\! Will shut down at $MIN_BATTERY%."
+    [ -e /tmp/running_low ] && [ $BATTERY -le $MIN_BATTERY ] && halt -p
+fi
+
+echo $BATTERY
