312dca9061b7e619bd3a38ab6fd16b9470e1fd54
cphyc <admin@cphyc.me>
2014-09-29 10:44:05 +0200

    REquires adm-tuned (from Fedora) and let's you toggle any of the performance preset

---
+++ power-profile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# you can change for askroot, gksudo, sudo
+SUDOER=gksudo
+profiles=$(tuned-adm list | awk -F"- " '$1=="" { print $2 }')
+profile_current=$(tuned-adm active | awk -F': ' '{ print $2 }')
+profiles_bool=$(
+    for p in $profiles; do 
+	[ $p = $profile_current ] && echo TRUE $p || echo FALSE $p
+    done
+)
+profile=$(
+    zenity --list --text="$activeProfile" --column='Pick' --column='Behaviour'\
+    --radiolist --hide-header $profiles_bool
+)
+ 	$SUDOER tuned-adm profile $profile #&& #2&> /dev/null &&
+ 	zenity --info --text="Successfully changed power plan, it is now \`$(tuned-adm active | awk -F': ' '{ print $2 }')\'" ||
+	zenity --info --text="An error occured, power plan not changed" 
+
