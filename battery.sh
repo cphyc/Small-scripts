73bd304ca7a598596dcd68a767e5d8bebb6d7560
cphyc <admin@cphyc.me>
2014-11-01 14:50:30 +0100

    Refactoring to support 3 states : warning, low and critical

--- battery.sh
+++ battery.sh
@@ -10,17 +10,27 @@
 # (if he wants to).                                #
 ####################################################
 
-MIN_BATTERY=5
-WARN_BATTERY=10
+BATTERY_LOW=7
+BATTERY_CRITICAL=5
+BATTERY_WARN=10
 BATTERY=$(acpi -b | cut -d ',' -f 2 | cut -d '%' -f 1 | cut -d " " -f 2)
 BATTERY_PREV=$([ -e /tmp/running_low ] && cat /tmp/running_low || 0)
 BATTERY_STATE=$(acpi -b | cut -d ',' -f 1 | cut -d ':' -f 2 | cut -d " " -f 2)
+NOTIF=notify-send
 
 if [ $BATTERY_STATE == "Discharging" ]; then
-    # Checks that the battery hasnot reached the minimum
-    [ $BATTERY -lt $WARN_BATTERY ] && echo $BATTERY > /tmp/running_low &&
-    [ $BATTERY -lt $BATTERY_PREV ] && zenity --info --text="Battery low ($BATTERY%)\! Will shut down at $MIN_BATTERY%."
-    [ -e /tmp/running_low ] && [ $BATTERY -le $MIN_BATTERY ] && halt -p
+    # Checks that the battery hasnot reached the minimu
+    if [ $BATTERY -lt $BATTERY_WARN ] ; then
+	echo $BATTERY > /tmp/running_low
+    fi
+    if [ $BATTERY -lt $BATTERY_PREV ]; then
+	if [ $BATTERY -le $BATTERY_LOW]; then
+	    notify-send "Battery critically low ($BATTERY%)\! Will shut down at $BATTERY_LOW%."
+	else 
+	    notify-send "Battery  low ($BATTERY%)\! Will shut down at $BATTERY_LOW%."
+	fi
+    fi
+    [ $BATTERY -le $BATTERY_CRITICAL ] && halt -p
 fi
 
 echo $BATTERY
