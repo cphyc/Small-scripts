81a8b2fdf0aa581629ca31faeb685cdad93f9160
Corentin Cadiou <contact@cphyc.me>
2015-12-03 22:04:07 +0100

    Remove username and password and reformat code

--- sms.py
+++ sms.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python2
 
 import sys
+import requests
+
+CONFIG = {
+    "user": "CONFIGUREME",
+    "pass": "CONFIGUREME"
+}
 
 if __name__ == "__main__" :
     # get message from argument
-    message = sys.argv[1:]
+    message = " ".join(sys.argv[1:])
 
     #create the url
-    url = r"https://smsapi.free-mobile.fr/sendmsg?user=14639048&pass=bUQgagQNfkHEGh&msg="+" ".join(message)
-
-    # convert into url
-    
+    url = r"https://smsapi.free-mobile.fr/sendmsg?user={}&pass={}&msg={}".format(CONFIG['user'], CONFIG['pass'], message)
     
     # request the page
-    # TODO: check hy it's necessary to verify ...
-    import requests
     r = requests.get(url, verify=False)
 
     # check the outcode
-    if r.status_code <> 200:
-        raise Exception("Error - status code %s"%r.status_code)
+    if r.status_code != 200:
+        raise Exception("Error - status code {}".format(r.status_code))
     
